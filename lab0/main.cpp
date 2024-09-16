d8a639b00d3db696d754294b748fcfc4272e92b5
ggewo <1gewe1111@gmail.com>
2024-09-16 12:58:32 +0300

    Create hello world!

---
+++ lab0/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string inptext;
+    cout << "Введите текст: ";
+    cin >> inptext; 
+    cout << "Вы ввели: " << inptext << endl;
+    return 0;
+}
